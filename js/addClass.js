ddee546257023acdcaa77257175a9d18db2a4649
LQQ <lqq201194@163.com>
2016-12-09 17:18:29 +0800

    simply addClass

--- js/addClass.js
+++ js/addClass.js
@@ -1,23 +1,23 @@
 $(function(){
   //UpLoad
   var upLoadInput = $(".desc form").find("input").eq(4);
-  upLoadInput.addClass("button").addClass("green");
+  upLoadInput.addClass("button green");
   //homework lists :Download and Delete
   var downloadInput = $("#list").find("input").eq(1);
   var DeleteInput = $("#list").find("input").eq(2);
-  downloadInput.addClass("button").addClass("green");
-  DeleteInput.addClass("button").addClass("red");
+  downloadInput.addClass("button green");
+  DeleteInput.addClass("button red");
   //Assignments :Download
   var AssignmentsdownloadInput = $("#assignments").find("input").eq(3);
-  AssignmentsdownloadInput.addClass("button").addClass("green");
+  AssignmentsdownloadInput.addClass("button green");
   //Hello part
   var signOut = $(".recent").find("input").eq(0);
   var Change = $(".recent").find("input").eq(2);
-  signOut.addClass("button").addClass("blue").addClass("small");
-  Change.addClass("button").addClass("red").addClass("small");
+  signOut.addClass("button blue small");
+  Change.addClass("button red small");
   //h3 color and font-size
   $(".comment-list").find("h3").each(function(){
-    $(this).css("color","#2AD2C9").css("font-size","25pt");
+    $(this).css({"color":"#2AD2C9","font-size":"25pt"})
   });
   $(".comment-list").find("h3").eq(1).css("padding","28px");
 })
