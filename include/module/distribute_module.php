9c90384167b4b999cbe06134571aaf5ddd64e1f5
Brayan Tang <tangbingxiao@foxmail.com>
2016-12-19 22:56:14 +0800

    Add DistributeModule For Admin Console

--- include/module/distribute_module.php
+++ include/module/distribute_module.php
@@ -2,23 +2,22 @@
     include_once "include/module/module.php";
     include_once "include/common/log.php";
     include_once "include/common/fun.php";
-    include_once "include/common/user.php";
     include_once "include/common/web.php";
-    //include_once "include/service/upload_service.php";
 
-    //submit what you want
-    class SubmitModule implements Module {
+    //distribute new assignment
+    class DistributeModule implements Module {
         private $spaceNum;
         private $assignDir;
-        private $user;
-        static private $FILENAME = "Submit_FileName";
-        static private $UPLOAD   = "Submit_Upload";
-        static private $SAVEFILENAME = "Submit_Select";
 
-        public function __construct($spaceNum, $assignDir, $user) {
-            $this->spaceNum  = $spaceNum;
-            $this->assignDir = File::Trim($assignDir);
-            $this->user      = $user;
+        static private $FILENAME = "Distribute_FileName";
+        static private $UPLOAD   = "Distribute_Upload";
+
+        static private $SAVEFILENAME = "Distribute_No";
+
+        public function __construct($spaceNum) {
+            $this->spaceNum = $spaceNum;
+            $this->assignDir = File::Trim(Configure::$ADMIN_DIR)."/Assignment";
+            File::Mkdir($this->assignDir);
         }
 
         static public function GetFileName() {
@@ -33,30 +32,33 @@
             return self::$SAVEFILENAME;
         }
 
+        public function GetAssignDir() {
+            return $this->assignDir;
+        }
+
         public function Display() {
             $prefix = Fun::NSpaceStr($this->spaceNum);
             $RETURN_VALUE_CONTAIN_SUBDIR = false;
             $files = File::LS($this->assignDir, $RETURN_VALUE_CONTAIN_SUBDIR);
-            if ( 0 == count($files) ) {
-                Log::Echo2Web($prefix."<p>You can't submit your homework because no assignment available.</p>");
-                return;
-            }
-
             $str      = $prefix."<form enctype = \"multipart/form-data\" action = \"".
                         Web::GetCurrentPage().
                         "\" method = \"post\">\n";
-            $str      .= $prefix."    <p>Choose No.</p>\n";
+            $str     .= $prefix."    <p>This is No.</p>\n";
             $size = count($files);
             for ( $i = 1; $i <= $size; $i++ ) {
                 $str .= $prefix."    <input type = \"radio\" name = \"".
-                        self::$SAVEFILENAME."\" value = \"".
-                        $this->user->GetId()."_No_".$i."\" required>$i\n";
+                        self::$SAVEFILENAME."\" value = \"《概率论与随机过程》大作业".
+                        $i."\" required>$i\n";
             }
-            $str     .= $prefix."    <p>Assignment to submit.</p><br>\n";
+            $size++;
+            $str     .= $prefix."    <input type = \"radio\" name = \"".
+                        self::$SAVEFILENAME."\" value = \"《概率论与随机过程》大作业".
+                        $size."\" checked = \"true\" required>$size (New)\n";
+            $str     .= $prefix."    <p>assignment to distribute</p>\n";
             $str     .= $prefix."    <input type = \"file\" name = \"".
                         self::$FILENAME."\" required><br>\n";
             $str     .= $prefix."    <input type = \"submit\" name = \"".
-                        self::$UPLOAD."\" value = \"Upload\">\n";
+                        self::$UPLOAD."\" value = \"Distribute\">\n";
             $str     .= $prefix."</form>\n";
             Log::RawEcho($str);
         }
--- test.php
+++ test.php
@@ -1,23 +1,10 @@
-<?php
-    include_once "include/common/web.php";
-    include_once "include/service/session_service.php";
-    $url = $_SERVER["PHP_SELF"];
-    $s = new SessionService($url);
-    $s->Run();
-?>
 <!DOCTYPE html>
 <body>
 <p>abcv</p>
 <?php
-    include_once "include/module/login_form_module.php";
-    include_once "include/service/login_service.php";
-    $m = new LoginFormModule(2);
+    include_once "include/module/distribute_module.php";
+    $m = new DistributeModule(2);
     $m->Display();
-
-    $ls = new LoginService(LoginFormModule::GetLoginButton(),
-                           LoginFormModule::GetUsername(),
-                           LoginFormModule::GetPassword());
-    $ls->Run();
 ?>
 <p>dfgsdfg</p>
 </body>
